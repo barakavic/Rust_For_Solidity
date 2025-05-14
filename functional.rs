510ce7404b58a6210aabf8ee2a03cbb7e5d95efa
barakavic <barakavictor397@gmail.com>
2025-05-14 19:58:55 +0300

    Dealt with pure and high order functions

--- functional.rs
+++ functional.rs
@@ -3,4 +3,31 @@
 pub fn add(x: i32, y: i32) -> i32{
     x+y//The function is pure same input gives the same output
 
+    //The -> specifies the return datatype
+
+
+    //pub is the access modifier for allowing for public access
+}
+
+//Higher order functions
+pub fn apply(value: i32, func: fn(i32)->i32)->i32{
+    println!("Inside apply({}, function", value);
+    func(value)
+}
+
+pub fn increment(n: i32) -> i32{
+    println!("Inside increment({})",n);
+    n+1
+}
+
+
+pub fn decrement(n: i32) -> i32{
+    println!("Inside decrement({})",n);
+    n-1
+}
+
+//Example of a function used with apply
+pub fn square(n: i32)-> i32{
+    println!("Inside Square ({})",n);
+    n*n
 }
--- main.rs
+++ main.rs
@@ -8,8 +8,20 @@ fn main(){
     let a = 23;
     let b = 45;
     let sum = functional::add(a, b);
-    println!("The sum of{} and {} is :{}", a, b, sum);
-    
+    println!("The sum of {} and {} is :{}", a, b, sum);
 
+
+
+
+    println!("\n ---Higher Order Function---");
+    let num = 10;
+    let incremented = functional::apply(num, functional::increment);
+    println!("{} incremented is {}", num, incremented);
+
+    let decremented = functional::apply(num, functional::decrement);
+    println!("\n{} decremented is: {}",num, decremented);
+
+    let squared = functional::apply(num,functional::square);
+    println!("{} squared is: {}", num, squared);
 
 }
